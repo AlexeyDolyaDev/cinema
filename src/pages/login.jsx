f61b1238d58f44d64c49388678f00567cad9a446
Alexey Dolya <a.dolya.spdload@gmail.com>
2022-09-15 00:16:41 +0300

    update

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,26 @@
-import { Link } from "react-router-dom";
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+    email: yup.string().required().email('Invalid email'),
+    password: yup.string().required().min(3).max(20),
+}).required();
 
 function Login() {
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+    });
+    console.log('ERRORS', errors)
     return (
         <>
             <h1>This is login page!</h1>
-            <nav>
-                <Link to="/sign-up">About</Link>
-            </nav>
+            <form onSubmit={handleSubmit(data => console.log(data))}>
+                <input {...register("email")} />
+                {errors && errors.email ? <h4 style={{color: 'red'}}>{errors.email.message}</h4> : <></>}
+                <input {...register("password")} />
+                <input type="submit" />
+            </form>
         </>
     );
 }
